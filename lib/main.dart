eda739d8cfae06b376a89728ab1c830268079d4d
student-deven <96278082+student-deven@users.noreply.github.com>
2021-12-17 14:08:45 +0700

    merusak code

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
